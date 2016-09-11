fdb1974db8b4b27f0b9ca0db82923edbe158516d
Martin Wiesner <martin4002@gmail.com>
2016-09-11 18:43:25 +0200

    Added onClick

--- app/src/main/java/me/martinwiesner/calculator/MainActivity.java
+++ app/src/main/java/me/martinwiesner/calculator/MainActivity.java
@@ -1,23 +1,52 @@
 package me.martinwiesner.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String result;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        result = (TextView) findViewById(R.id.text_result);
     }
 
     public void onClick(View view) {
+        String text = ((Button) view).getText().toString();
+        switch (text) {
+            case "<-":
+                result.setText(result.getText().toString() + text);
+                break;
+            case "/":
+                result.setText(result.getText().toString() + text);
+                break;
+            case "*":
+                result.setText(result.getText().toString() + text);
+                break;
+            case "-":
+                result.setText(result.getText().toString() + text);
+                break;
+            case "+":
+                result.setText(result.getText().toString() + text);
+                break;
+            case "=":
 
+                break;
+            default:
+                try {
+                    int number = Integer.parseInt(text);
+                    result.setText(result.getText().toString() + number);
+                } catch (NumberFormatException ex) {
+                    ex.printStackTrace();
+                }
+        }
     }
 
 }
