4859fc15241b8dea2f23d3578f2c97c09e523bfe
Martin Wiesner <martin4002@gmail.com>
2016-09-11 17:48:08 +0200

    Test

--- app/src/main/java/me/martinwiesner/calculator/MainActivity.java
+++ app/src/main/java/me/martinwiesner/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package me.martinwiesner.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("TAG", "Test");
     }
 
     public void onClick(View view) {
